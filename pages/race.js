05aaaa14af7a1ca46a9e24f6465f10460cb69338
Jon Heslop <jon@jonheslop.com>
2019-05-31 11:22:35 +0100

    Removing top riders component as it’s breaking Now v2
    No idea why yet, but none of that works as we don’t have a scapey server running so will just remove this so we can transition and work it out later

--- pages/race.js
+++ pages/race.js
@@ -12,7 +12,6 @@ import KeyEvents from '../components/key-events';
 import MapContainer from '../components/map-container';
 import Page from '../components/shared/page';
 import Post from '../components/post';
-import TopRiders from '../components/top-riders';
 import FactFile from '../components/fact-file';
 import Tabs from '../components/tabs';
 import Community from '../components/community'
@@ -216,7 +215,6 @@ class Race extends React.Component {
 				/>
 				<MapContainer raceID={this.props.trackleadersID}/>
 				<KeyEventsWrapper fl ph3 ph4_ns pb2 w_100 w_30_m w_20_l mt4_l relative id="events-wrap">
-					<TopRiders race={this.props.race}/>
 					<FactFile race={this.props.race}/>
 					<KeyEvents posts={this.props.posts} skip={this.state.skip}/>
 				</KeyEventsWrapper>
