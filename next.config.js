db8971a0c090f205e1ac6dd134c2f675e7c03762
Jon Heslop <jon@jonheslop.com>
2019-06-14 15:23:47 +0100

    Dont need any webpack config for secrets now

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-const webpack = require('webpack');
-
 module.exports = {
-	target: 'serverless',
-	webpack: config => {
-		config.plugins.push(
-			new webpack.EnvironmentPlugin(process.env)
-		);
-		return config;
-	}
+	target: 'serverless'
 };
