214171535ff3c03b3fae77780dd8fc01983a9c0c
Jon Heslop <jon@jonheslop.com>
2019-07-05 16:53:12 +0100

    Using React Autosuggest to add filter box to the results page

--- components/results-index/index.js
+++ components/results-index/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import tachyons from 'styled-components-tachyons';
 import ResultsSummary from '../results-summary';
 import ResultsContribute from '../results-contribute';
+import ResultsFilter from './results-filter';
 
 const Div = styled.div`${tachyons}`;
 const Header = styled.header`${tachyons}`;
@@ -13,23 +14,43 @@ const Grid = styled.div`
 	grid-gap: var(--spacing-large);
 ${tachyons}`;
 
-const ResultsIndex = ({ raceResultsByYear }) => {
-	return (
-		<Div mt3 mt4_l mh6_l>
-			<Div>
-				<Header ma3>
-					<H1 ma0 f1 fw6>Browse race results</H1>
-				</Header>
-				<Grid mh3 pb4 bb bw1 b__light_gray>
-					{
-						raceResultsByYear.map((result, index) => <ResultsSummary event={result} key={index} />)
-					}
-				</Grid>
-				<ResultsContribute />
+
+class ResultsIndex extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			filteredEvent: ''
+		};
+		this.setFilteredEvent = this.setFilteredEvent.bind(this);
+	}
+
+	setFilteredEvent(id) {
+		this.setState({
+			filteredEvent: id
+		});
+	}
+
+	render() {
+		return (
+			<Div mt3 mt4_l mh6_l>
+				<Div>
+					<Header ma3>
+						<H1 ma0 f1 fw6>Browse race results</H1>
+					</Header>
+					<Div>
+						<ResultsFilter events={this.props.raceResultsByYear} setFilteredEvent={this.setFilteredEvent.bind(this)} />
+					</Div>
+					<Grid mh3 pb4 bb bw1 b__light_gray>
+						{
+							this.props.raceResultsByYear.map((result, index) => <ResultsSummary event={result} key={index} filtered={this.state.filteredEvent} />)
+						}
+					</Grid>
+					<ResultsContribute />
+				</Div>
 			</Div>
-		</Div>
-	);
-};
+		);
+	};
+}
 
 ResultsIndex.propTypes = {
 	raceResultsByYear: PropTypes.array
