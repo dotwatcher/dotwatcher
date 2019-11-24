8a17a19819e1f1b12e3b6c28386c54bc255e3aad
Alex Windett <windettalex@gmail.com>
2019-11-24 23:08:31 +0000

    add fuzzy search on type for race name

--- components/results-index/index.js
+++ components/results-index/index.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import tachyons from 'styled-components-tachyons';
-import ResultsSummary from '../results-summary';
-import ResultsContribute from '../results-contribute';
-import ResultsFilter from './results-filter';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import tachyons from "styled-components-tachyons";
+import ResultsSummary from "../results-summary";
+import ResultsContribute from "../results-contribute";
+import ResultsFilter from "./results-filter";
 
-const Div = styled.div`${tachyons}`;
-const Header = styled.header`${tachyons}`;
-const H1 = styled.h1`${tachyons}`;
+const Div = styled.div`
+	${tachyons}
+`;
+const Header = styled.header`
+	${tachyons}
+`;
+const H1 = styled.h1`
+	${tachyons}
+`;
 const Grid = styled.div`
 	display: grid;
 	grid-gap: var(--spacing-large);
-${tachyons}`;
-
+	${tachyons}
+`;
 
 class ResultsIndex extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			filteredEvent: ''
+			filteredEvent: ""
 		};
 		this.setFilteredRace = this.setFilteredRace.bind(this);
 	}
@@ -31,25 +37,36 @@ class ResultsIndex extends Component {
 	}
 
 	render() {
+		console.log("results list");
 		return (
 			<Div mt3 mt4_l mh6_l>
 				<Div>
 					<Header ma3>
-						<H1 ma0 f1 fw6>Browse race results</H1>
+						<H1 ma0 f1 fw6>
+							Browse race results
+						</H1>
 					</Header>
 					<Div>
-						<ResultsFilter races={this.props.allRaces} setFilteredRace={this.setFilteredRace.bind(this)} />
+						<ResultsFilter
+							races={this.props.allRaces}
+							setFilteredRace={this.setFilteredRace.bind(this)}
+							handleSearchUpdate={this.props.handleSearchUpdate}
+						/>
 					</Div>
 					<Grid mh3 pb4 bb bw1 b__light_gray>
-						{
-							this.props.allRaces.map((race, index) => <ResultsSummary race={race} key={index} filtered={this.state.filteredEvent} />)
-						}
+						{this.props.allRaces.map((race, index) => (
+							<ResultsSummary
+								race={race}
+								key={index}
+								filtered={this.state.filteredEvent}
+							/>
+						))}
 					</Grid>
 					<ResultsContribute />
 				</Div>
 			</Div>
 		);
-	};
+	}
 }
 
 ResultsIndex.propTypes = {
--- components/results-index/results-filter.js
+++ components/results-index/results-filter.js
@@ -1,152 +1,167 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Autosuggest from 'react-autosuggest';
-import styled from 'styled-components';
-import tachyons from 'styled-components-tachyons';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Autosuggest from "react-autosuggest";
+import styled from "styled-components";
+import tachyons from "styled-components-tachyons";
 
-const Clear = styled.button`${tachyons}`;
+const Clear = styled.button`
+	${tachyons}
+`;
 
 const theme = {
-  container: {
-    display: 'inline-block',
-    position: 'relative',
-    margin: '0 1rem 1rem' 
-  },
-  input: {
-    width: 300,
-    padding: '.5rem .75rem',
-    fontSize: '1.25rem',
-    border: '.125rem solid #1b1919',
-  },
-  inputFocused: {
-    outline: 'none'
-  },
-  inputOpen: {
-  },
-  suggestionsContainer: {
-    display: 'none'
-  },
-  suggestionsContainerOpen: {
-    display: 'block',
-    position: 'absolute',
-    top: 40,
-    width: 300,
-    border: '.125rem solid #1b1919',
-    backgroundColor: '#fff',
-    fontSize: '1.25rem',
-    zIndex: 2
-  },
-  suggestionsList: {
-    margin: 0,
-    padding: 0,
-    listStyleType: 'none',
-  },
-  suggestion: {
-    cursor: 'pointer',
-    padding: '.5rem .75rem',
-  },
-  suggestionHighlighted: {
-    backgroundColor: '#fbf1a9'
-  }
+	container: {
+		display: "inline-block",
+		position: "relative",
+		margin: "0 1rem 1rem"
+	},
+	input: {
+		width: 300,
+		padding: ".5rem .75rem",
+		fontSize: "1.25rem",
+		border: ".125rem solid #1b1919"
+	},
+	inputFocused: {
+		outline: "none"
+	},
+	inputOpen: {},
+	suggestionsContainer: {
+		display: "none"
+	},
+	suggestionsContainerOpen: {
+		display: "block",
+		position: "absolute",
+		top: 40,
+		width: 300,
+		border: ".125rem solid #1b1919",
+		backgroundColor: "#fff",
+		fontSize: "1.25rem",
+		zIndex: 2
+	},
+	suggestionsList: {
+		margin: 0,
+		padding: 0,
+		listStyleType: "none"
+	},
+	suggestion: {
+		cursor: "pointer",
+		padding: ".5rem .75rem"
+	},
+	suggestionHighlighted: {
+		backgroundColor: "#fbf1a9"
+	}
 };
 
 // https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
 function escapeRegexCharacters(str) {
-  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+	return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
 
 function getSuggestionValue(suggestion) {
-  return suggestion.name;
+	return suggestion.name;
 }
 
 function renderSuggestion(suggestion) {
-  return (
-    <span>{suggestion.name}</span>
-  );
+	return <span>{suggestion.name}</span>;
 }
-
 
 class Filter extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      value: '',
-      suggestions: []
-    };
-  }
+		this.state = {
+			value: "",
+			suggestions: []
+		};
+	}
 
-  onChange = (event, { newValue, method }) => {
-    this.setState({
-      value: newValue
-    });
-  };
+	onChange = async (event, { newValue, method }) => {
+		await this.setState({
+			value: newValue
+		});
 
-  onSuggestionsFetchRequested = ({ value }) => {
-    this.setState({
-      suggestions: this.getSuggestions(value)
-    });
-  };
+		this.props.handleSearchUpdate(newValue);
+	};
 
-  onSuggestionsClearRequested = () => {
-    this.setState({
-      suggestions: []
-    });
-  };
+	onSuggestionsFetchRequested = ({ value }) => {
+		this.setState({
+			suggestions: this.getSuggestions(value)
+		});
+	};
 
-  onSuggestionSelected = (event, opts) => {
-    this.props.setFilteredRace(opts.suggestion.name)
-  };
+	onSuggestionsClearRequested = () => {
+		this.setState({
+			suggestions: []
+		});
+	};
 
-  getSuggestions = value => {
-    const escapedValue = escapeRegexCharacters(value.trim());
+	onSuggestionSelected = (event, opts) => {
+		this.props.setFilteredRace(opts.suggestion.name);
+	};
 
-    if (escapedValue === '') {
-      return [];
-    }
+	getSuggestions = value => {
+		const escapedValue = escapeRegexCharacters(value.trim());
 
-    const regex = new RegExp(escapedValue, 'i');
+		if (escapedValue === "") {
+			return [];
+		}
 
-    return this.props.races.filter(event => regex.test(event.name));
-  }
-  
-  clear = () => {
-    this.props.setFilteredRace('')
-    this.setState({
-      value: ''
-    });
-  }
+		const regex = new RegExp(escapedValue, "i");
 
-  render() {
-    const { value, suggestions } = this.state;
-    const inputProps = {
-      placeholder: "Find a race",
-      value,
-      onChange: this.onChange
-    };
+		return this.props.races.filter(event => regex.test(event.name));
+	};
 
-    return (
-      <React.Fragment>
-        <Autosuggest
-          suggestions={suggestions}
-          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-          getSuggestionValue={getSuggestionValue}
-          renderSuggestion={renderSuggestion}
-          inputProps={inputProps}
-          onSuggestionSelected={this.onSuggestionSelected}
-          highlightFirstSuggestion={true}
-          theme={theme} />
-        <Clear input_reset bg_light_gray hover_bg_lightest_blue f5 tracked bn ttu ph3 pv2 lh_solid dib onClick={this.clear}>Clear</Clear>
-      </React.Fragment>
-    );
-  }
+	clear = () => {
+		this.props.setFilteredRace("");
+		this.setState({
+			value: ""
+		});
+	};
+
+	render() {
+		const { value, suggestions } = this.state;
+		const inputProps = {
+			placeholder: "Find a race",
+			value,
+			onChange: this.onChange
+		};
+
+		return (
+			<React.Fragment>
+				<Autosuggest
+					suggestions={suggestions}
+					onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+					onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+					getSuggestionValue={getSuggestionValue}
+					renderSuggestion={renderSuggestion}
+					inputProps={inputProps}
+					onSuggestionSelected={this.onSuggestionSelected}
+					highlightFirstSuggestion={true}
+					theme={theme}
+				/>
+				<Clear
+					input_reset
+					bg_light_gray
+					hover_bg_lightest_blue
+					f5
+					tracked
+					bn
+					ttu
+					ph3
+					pv2
+					lh_solid
+					dib
+					onClick={this.clear}
+				>
+					Clear
+				</Clear>
+			</React.Fragment>
+		);
+	}
 }
 
-export default Filter
+export default Filter;
 
 Filter.propTypes = {
-  races: PropTypes.array,
-  setFilteredRace: PropTypes.func
+	races: PropTypes.array,
+	setFilteredRace: PropTypes.func
 };
-
