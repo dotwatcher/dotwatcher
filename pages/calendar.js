1f5fb049682197c8c14ad8851b2b7d0cbc6e2f8c
Alex Windett <windettalex@gmail.com>
2019-12-07 12:51:28 +0000

    add meta date to HTML Head

--- pages/calendar.js
+++ pages/calendar.js
@@ -27,7 +27,16 @@ const CalanderPage = ({ races = [] }) => {
 	return (
 		<Page>
 			<Head>
-				<title>Calander – DotWatcher.cc</title>
+				<title>Calendar - DotWatcher.cc</title>
+				<meta property="og:title" content="Calendar - DotWatcher.cc" />
+				<meta
+					property="og:description"
+					content="DotWatcher is here to showcase the best of long distance self-supported bike racing."
+				/>
+				<meta
+					property="og:image"
+					content="https://images.ctfassets.net/6hyijb95boju/KQ7Yj247Go6KOIm60SeQ2/9315aa310eee6a72088c9c37de8aa1e6/DotWatcher---Logo---Pin-_1_.jpg"
+				/>
 			</Head>
 
 			<Header title="dotwatcher.cc" />
