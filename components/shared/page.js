8cbb888c7734797ec8d4f6178d9835bef819448c
Alex Windett <windettalex@gmail.com>
2020-01-13 21:53:20 +0000

    add newsletter sign up modal + cookies

--- components/shared/page.js
+++ components/shared/page.js
@@ -1,13 +1,83 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import tachyons from 'styled-components-tachyons';
-import {initGA, logPageView} from '../../utils/analytics';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import tachyons from "styled-components-tachyons";
+import EmailSignUp from "../Newsletter";
+import { initGA, logPageView } from "../../utils/analytics";
+import Cookies from "js-cookie";
 
-const Div = styled.div`${tachyons}`;
+const Wrapper = styled.div`
+	${tachyons}
+	position: relative;
+`;
+
+const H2 = styled.h2`
+	${tachyons}
+`;
+
+const P = styled.p`
+	${tachyons}
+`;
+
+const Newsletter = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	height: 100vh;
+	z-index: 2;
+	display: grid;
+	grid-template-columns: repeat(12, 1fr);
+	align-items: center;
+
+	&:before {
+		content: "";
+		display: inline-block;
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+		left: 0;
+		background-color: rgba(62, 62, 62, 0.7);
+	}
+`;
+
+const NewsletterWrapper = styled.div`
+	grid-column: 3 / span 8;
+	z-index: 1;
+	padding: var(--spacing-extra-large);
+	background: var(--white);
+	position: relative;
+`;
+
+const Close = styled.button`
+	position: absolute;
+	top: 20px;
+	right: 20px;
+	background: none;
+	border: none;
+`;
 
 class Page extends Component {
+	constructor(props) {
+		super(props);
+
+		this.COOKIE_NAME = "newsletterModal";
+
+		this.state = {
+			showModal: false
+		};
+
+		this.setCookie = this.setCookie.bind(this);
+	}
+
 	componentDidMount() {
+		if (!Cookies.get(this.COOKIE_NAME)) {
+			this.setState({
+				showModal: true
+			});
+		}
+
 		if (!window.GA_INITIALIZED) {
 			initGA();
 			window.GA_INITIALIZED = true;
@@ -15,11 +85,37 @@ class Page extends Component {
 		logPageView();
 	}
 
+	setCookie() {
+		Cookies.set(this.COOKIE_NAME, true);
+
+		this.setState({
+			showModal: false
+		});
+	}
+
 	render() {
 		return (
-			<Div sans_serif near_black pa0 ma0 className="cf">
+			<Wrapper sans_serif near_black pa0 ma0 className="cf">
 				{this.props.children}
-			</Div>
+
+				{this.state.showModal && (
+					<Newsletter>
+						<NewsletterWrapper>
+							<H2 ma0 mb3>
+								The DotWatcher Digest
+							</H2>
+							<P lh-copy ma0 mb3>
+								For all the latest updates from the bikepacking webosphere and
+								beyond, sign up to DotWatcher Digest.
+							</P>
+							<Close type="button" onClick={this.setCookie}>
+								X
+							</Close>
+							<EmailSignUp showPastIssues={false} onSubmit={this.setCookie} />
+						</NewsletterWrapper>
+					</Newsletter>
+				)}
+			</Wrapper>
 		);
 	}
 }
@@ -29,7 +125,7 @@ Page.propTypes = {
 };
 
 Page.defaultProps = {
-	children: ''
+	children: ""
 };
 
 export default Page;
