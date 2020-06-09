ebf9f77af277cfc45004ccff2dc0486b078f4e4d
Alex Windett <windettalex@gmail.commit>
2020-06-09 08:41:11 +0100

    again

--- utils/contstants.js
+++ utils/contstants.js
@@ -1,2 +1,1 @@
-export const AUTH0_CALLBACK_DOMAIN =
-	process.env.AUTH0_CALLBACK_DOMAIN || `https://${process.env.VERCEL_URL}`;
+export const AUTH0_CALLBACK_DOMAIN = `https://${process.env.VERCEL_URL}`;
