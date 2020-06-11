75a2f768de326397893b45918e17eaea96fde79f
Alex Windett <windettalex@gmail.commit>
2020-06-11 21:05:11 +0100

    add callback domain overrride

--- utils/contstants.js
+++ utils/contstants.js
@@ -1,1 +1,2 @@
-export const AUTH0_CALLBACK_DOMAIN = `https://${process.env.VERCEL_URL}`;
+export const AUTH0_CALLBACK_DOMAIN =
+	process.env.AUTH0_CALLBACK_DOMAIN || `https://${process.env.VERCEL_URL}`;
