62237e882f9901e4d9d991360be14d65ed5a4d27
Alex Windett <windettalex@gmail.commit>
2020-06-30 09:44:33 +0100

    undo log out changes

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -1,14 +1,14 @@
 import auth0 from "../../../lib/auth0";
 
 export default async function logout(req, res) {
-	// try {
-	await auth0.handleLogout(req, res);
-	// } catch (error) {
-	// 	res.writeHead(302, {
-	// 		Location:
-	// 			"/login/error?errorLocation=auth/logout&errorLogs=" +
-	// 			JSON.stringify(error)
-	// 	});
-	// 	res.end();
-	// }
+	try {
+		await auth0.handleLogout(req, res);
+	} catch (error) {
+		res.writeHead(302, {
+			Location:
+				"/login/error?errorLocation=auth/logout&errorLogs=" +
+				JSON.stringify(error)
+		});
+		res.end();
+	}
 }
