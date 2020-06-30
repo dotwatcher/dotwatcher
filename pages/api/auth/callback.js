7582040201d2d238deba04c240001ba079713190
Alex Windett <windettalex@gmail.commit>
2020-06-30 09:40:33 +0100

    redirect callback to login

--- pages/api/auth/callback.js
+++ pages/api/auth/callback.js
@@ -1,22 +1,21 @@
 import auth0 from "../../../lib/auth0";
+import Sentry from "@sentry/browser";
 
 export default async function callback(req, res) {
 	const { profile } = req.cookies;
 
 	const redirectTo = profile || "/profile/edit";
 
-	// try {
-	await auth0.handleCallback(req, res, {
-		getState: req => {
-			return { redirectTo };
-		}
-	});
-	// } catch (error) {
-	// 	res.writeHead(302, {
-	// 		Location:
-	// 			"/login/error?errorLocation=auth/callback&errorLogs=" +
-	// 			JSON.stringify(error)
-	// 	});
-	// 	res.end();
-	// }
+	try {
+		await auth0.handleCallback(req, res, { redirectTo });
+	} catch (error) {
+		await Sentry.captureMessage("Error in api/auth/callback", "critical");
+		res.writeHead(302, { Location: "/api/auth/login" });
+		// res.writeHead(302, {
+		// 	Location:
+		// 		"/login/error?errorLocation=auth/callback&errorLogs=" +
+		// 		JSON.stringify(error)
+		// });
+		res.end();
+	}
 }
--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,14 +1,16 @@
 import auth0 from "../../../lib/auth0";
+import Sentry from "@sentry/browser";
 
 export default async function login(req, res) {
-	// try {
-	await auth0.handleLogin(req, res);
-	// } catch (error) {
-	// 	res.writeHead(302, {
-	// 		Location:
-	// 			"/login/error?errorLocation=auth/login&errorLogs=" +
-	// 			JSON.stringify(error)
-	// 	});
-	// 	res.end();
-	// }
+	try {
+		await auth0.handleLogin(req, res);
+	} catch (error) {
+		await Sentry.captureMessage("Error in api/auth/login", "critical");
+		res.writeHead(302, {
+			Location:
+				"/login/error?errorLocation=auth/login&errorLogs=" +
+				JSON.stringify(error)
+		});
+		res.end();
+	}
 }
