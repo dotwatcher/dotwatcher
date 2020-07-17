cfcd7cbbd31c24c03bd10606e65b1d15ef28e39f
Alex Windett <windettalex@gmail.commit>
2020-07-17 08:19:32 +0100

    convert class to function components

--- pages/about/index.js
+++ pages/about/index.js
@@ -27,108 +27,100 @@ const H1 = styled.h1`
 	${tachyons}
 `;
 
-class AboutPage extends React.Component {
-	render() {
-		const StyledWrapper = styled.div`
-			background-image: ${this.props.page.image
-				? `url(${this.props.page.image.fields.file.url})`
-				: "none"};
+const StyledWrapper = styled.div`
+	${({ background }) =>
+		background &&
+		css`
+			background-image: url(${background});
 			background-repeat: no-repeat;
 			background-size: cover;
-			padding: ${this.props.page.image
-					? `var(--spacing-extra-extra-large)`
-					: ``}
-				0;
+		`};
 
-			& > div {
-				background-color: rgba(255, 255, 255, 0.66);
-			}
-			${tachyons}
-		`;
-
-		const carouselSlides = this.props.page.blocks.filter(
-			block => block.layout === "Carousel slide"
-		);
-		const carousel = carouselSlides.length ? (
-			<Carousel slides={carouselSlides} />
-		) : null;
-		const blocksWithoutSlides = this.props.page.blocks.filter(
-			block => block.layout !== "Carousel slide"
-		);
-		return (
-			<Page>
-				<Head>
-					<title>{this.props.page.title} – DotWatcher.cc</title>
-					<meta
-						property="og:title"
-						content={`${this.props.page.title} – DotWatcher.cc`}
-					/>
-					<meta
-						property="og:description"
-						content="DotWatcher is here to showcase the best of long distance self-supported bike racing."
-					/>
-					<meta
-						property="og:image"
-						content={
-							this.props.page.image
-								? this.props.page.image.fields.file.url
-								: "https://images.ctfassets.net/6hyijb95boju/KQ7Yj247Go6KOIm60SeQ2/9315aa310eee6a72088c9c37de8aa1e6/DotWatcher---Logo---Pin-_1_.jpg"
-						}
-					/>
-					<meta name="twitter:card" content="summary_large_image" />
-					<meta name="twitter:site" content="@dotwatcher" />
-					<meta name="twitter:creator" content="@dotwatcher" />
-					<meta
-						name="twitter:title"
-						content={`${this.props.page.title} – DotWatcher.cc`}
-					/>
-					<meta
-						name="twitter:description"
-						content="DotWatcher is here to showcase the best of long distance self-supported bike racing."
-					/>
-					<meta
-						property="og:image"
-						content={
-							this.props.page.image
-								? this.props.page.image.fields.file.url
-								: "https://images.ctfassets.net/6hyijb95boju/KQ7Yj247Go6KOIm60SeQ2/9315aa310eee6a72088c9c37de8aa1e6/DotWatcher---Logo---Pin-_1_.jpg"
-						}
-					/>
-					<meta
-						name="description"
-						content="DotWatcher is here to showcase the best of long distance self-supported bike racing."
-					/>
-				</Head>
-				<Header user={this.props.user} title="dotwatcher.cc" />
-				<StyledWrapper fl w_100>
-					<Div fl mt5_ns pa3 pa4_ns pl5_ns>
-						<H1 f2 f1_ns fw6 lh_solid mt0 mb4>
-							{widont(this.props.page.title)}
-						</H1>
-						<Div f4 f3_ns measure lh_copy>
-							<ReactMarkdown
-								source={this.props.page.text}
-								plugins={[shortcodes]}
-								renderers={{
-									shortcode: Embed,
-									link: AutoEmbed
-								}}
-							/>
-						</Div>
-					</Div>
-				</StyledWrapper>
-				<Div fl w_100 mt3 mt4_l cf>
-					{carousel}
-					{blocksWithoutSlides.map(block => {
-						return <ContentBlock key={block.sys.id} block={block} />;
-					})}
-					<ContributorsGrid contributors={this.props.contributors} />
-				</Div>
-				<Footer />
-			</Page>
-		);
+	& > div {
+		background-color: rgba(255, 255, 255, 0.66);
 	}
-}
+
+	${tachyons}
+`;
+
+const AboutPage = ({ page, user, contributors }) => {
+	const carouselSlides = page.blocks.filter(
+		block => block.layout === "Carousel slide"
+	);
+	const blocksWithoutSlides = page.blocks.filter(
+		block => block.layout !== "Carousel slide"
+	);
+
+	return (
+		<Page>
+			<Head>
+				<title>{page.title} – DotWatcher.cc</title>
+				<meta property="og:title" content={`${page.title} – DotWatcher.cc`} />
+				<meta
+					property="og:description"
+					content="DotWatcher is here to showcase the best of long distance self-supported bike racing."
+				/>
+				<meta
+					property="og:image"
+					content={
+						page.image
+							? page.image.fields.file.url
+							: "https://images.ctfassets.net/6hyijb95boju/KQ7Yj247Go6KOIm60SeQ2/9315aa310eee6a72088c9c37de8aa1e6/DotWatcher---Logo---Pin-_1_.jpg"
+					}
+				/>
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@dotwatcher" />
+				<meta name="twitter:creator" content="@dotwatcher" />
+				<meta name="twitter:title" content={`${page.title} – DotWatcher.cc`} />
+				<meta
+					name="twitter:description"
+					content="DotWatcher is here to showcase the best of long distance self-supported bike racing."
+				/>
+				<meta
+					property="og:image"
+					content={
+						page.image
+							? page.image.fields.file.url
+							: "https://images.ctfassets.net/6hyijb95boju/KQ7Yj247Go6KOIm60SeQ2/9315aa310eee6a72088c9c37de8aa1e6/DotWatcher---Logo---Pin-_1_.jpg"
+					}
+				/>
+				<meta
+					name="description"
+					content="DotWatcher is here to showcase the best of long distance self-supported bike racing."
+				/>
+			</Head>
+			<Header user={user} title="dotwatcher.cc" />
+			<StyledWrapper fl w_100>
+				<Div fl mt5_ns pa3 pa4_ns pl5_ns>
+					<H1 f2 f1_ns fw6 lh_solid mt0 mb4>
+						{widont(page.title)}
+					</H1>
+					<Div f4 f3_ns measure lh_copy>
+						<ReactMarkdown
+							source={page.text}
+							plugins={[shortcodes]}
+							renderers={{
+								shortcode: Embed,
+								link: AutoEmbed
+							}}
+						/>
+					</Div>
+				</Div>
+			</StyledWrapper>
+			<Div fl w_100 mt3 mt4_l cf>
+				{carouselSlides && carouselSlides.length > 0 && (
+					<Carousel slides={carouselSlides} />
+				)}
+
+				{blocksWithoutSlides.map(block => (
+					<ContentBlock key={block.sys.id} block={block} />
+				))}
+				<ContributorsGrid contributors={contributors} />
+			</Div>
+			<Footer />
+		</Page>
+	);
+};
 
 AboutPage.propTypes = {
 	page: PropTypes.object.isRequired
--- pages/contributor/[name].js
+++ pages/contributor/[name].js
@@ -57,145 +57,136 @@ const Profile = styled.div`
 	${tachyons}
 `;
 
-class App extends Component {
-	render() {
-		return (
-			<Page>
-				<Head>
-					<title>
-						{this.props.contributor.name}’s contributor profile - DotWatcher.cc
-					</title>
-					<meta
-						property="og:title"
-						content={`${this.props.contributor.name}’s contributor profile - DotWatcher.cc`}
-					/>
-					<meta
-						property="og:image"
-						content={`${this.props.contributor.avatar.url}?w=600&h=600&fm=jpg&q=80`}
-					/>
-					<meta name="twitter:card" content="summary_large_image" />
-					<meta name="twitter:site" content="@dotwatcher" />
-					<meta name="twitter:creator" content="@dotwatcher" />
-					<meta
-						name="twitter:title"
-						content={`${this.props.contributor.name}’s contributor profile - DotWatcher.cc`}
-					/>
-					<meta
-						name="twitter:image"
-						content={`${this.props.contributor.avatar.url}?w=600&h=600&fm=jpg&q=80`}
-					/>
-					<meta
-						name="description"
-						content={`${this.props.contributor.name}’s contributor profile`}
-					/>
-				</Head>
-				<Header user={this.props.user} title="dotwatcher.cc" />
-				<Div mt3 mh3 mh6_l>
-					<Contributor mt4 pb5 className="cf">
-						<Sidebar tc_ns w_50 w_100_ns>
-							<Figure ma0 mb3>
-								<Img
-									img
-									db
-									bg_light_gray
-									br_100
-									title={this.props.contributor.avatar.title}
-									alt={this.props.contributor.avatar.description}
-									src={`${this.props.contributor.avatar.url}?w=500&h=500&fit=fill&fm=jpg&q=60`}
-									srcSet={`${this.props.contributor.avatar.url}?w=500&h=500&fit=fill&fm=jpg&q=60 768w, ${this.props.contributor.avatar.url}?w=600&h=600&fit=fill&fm=jpg&q=80 1024w`}
-									sizes="200vw"
-								/>
-							</Figure>
-							{this.props.contributor.website ? (
-								<A
-									near_black
-									link
-									hover_blue
-									mb3
-									tl
-									tc_ns
-									fw6
-									db
-									href={this.props.contributor.website}
-								>
-									{this.props.contributor.website.replace(
-										/^(http|https):\/\//,
-										""
-									)}
-								</A>
-							) : null}
-							{this.props.contributor.instagramProfile ? (
-								<A
-									w2
-									mr3
-									mh3_ns
-									near_black
-									link
-									hover_blue
-									dib
-									v_btm
-									href={this.props.contributor.instagramProfile}
-									title={`Follow ${this.props.contributor.name} on Instagram`}
-								>
-									<InstagramLogo />
-								</A>
-							) : null}
-							{this.props.contributor.twitterProfile ? (
-								<A
-									w2
-									mh3
-									near_black
-									link
-									hover_blue
-									dib
-									v_btm
-									href={this.props.contributor.twitterProfile}
-									title={`Follow ${this.props.contributor.name} on Twitter`}
-								>
-									<TwitterLogo />
-								</A>
-							) : null}
-							{this.props.contributor.stravaProfile ? (
-								<A
-									w2
-									mh3
-									near_black
-									link
-									hover_blue
-									dib
-									v_btm
-									href={this.props.contributor.stravaProfile}
-									title={`Follow ${this.props.contributor.name} on Strava`}
-								>
-									<StravaLogo />
-								</A>
-							) : null}
-						</Sidebar>
-						<Profile>
-							<Heading mb3>
-								<H1 f3 f1_l fw6 lh_solid ma0>
-									{this.props.contributor.name}
-								</H1>
-							</Heading>
-							<Richtext source={this.props.contributor.bio} />
-							<Contribute />
-						</Profile>
-					</Contributor>
-				</Div>
-				<Footer />
-			</Page>
-		);
-	}
-}
+const ContributorPage = ({ contributor, user }) => (
+	<Page>
+		<Head>
+			<title>{contributor.name}’s contributor profile - DotWatcher.cc</title>
+			<meta
+				property="og:title"
+				content={`${contributor.name}’s contributor profile - DotWatcher.cc`}
+			/>
+			<meta
+				property="og:image"
+				content={`${contributor.avatar.url}?w=600&h=600&fm=jpg&q=80`}
+			/>
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:site" content="@dotwatcher" />
+			<meta name="twitter:creator" content="@dotwatcher" />
+			<meta
+				name="twitter:title"
+				content={`${contributor.name}’s contributor profile - DotWatcher.cc`}
+			/>
+			<meta
+				name="twitter:image"
+				content={`${contributor.avatar.url}?w=600&h=600&fm=jpg&q=80`}
+			/>
+			<meta
+				name="description"
+				content={`${contributor.name}’s contributor profile`}
+			/>
+		</Head>
+		<Header user={user} title="dotwatcher.cc" />
+		<Div mt3 mh3 mh6_l>
+			<Contributor mt4 pb5 className="cf">
+				<Sidebar tc_ns w_50 w_100_ns>
+					<Figure ma0 mb3>
+						<Img
+							img
+							db
+							bg_light_gray
+							br_100
+							title={contributor.avatar.title}
+							alt={contributor.avatar.description}
+							src={`${contributor.avatar.url}?w=500&h=500&fit=fill&fm=jpg&q=60`}
+							srcSet={`${contributor.avatar.url}?w=500&h=500&fit=fill&fm=jpg&q=60 768w, ${contributor.avatar.url}?w=600&h=600&fit=fill&fm=jpg&q=80 1024w`}
+							sizes="200vw"
+						/>
+					</Figure>
+					{contributor.website && (
+						<A
+							near_black
+							link
+							hover_blue
+							mb3
+							tl
+							tc_ns
+							fw6
+							db
+							href={contributor.website}
+						>
+							{contributor.website.replace(/^(http|https):\/\//, "")}
+						</A>
+					)}
+					{contributor.instagramProfile && (
+						<A
+							w2
+							mr3
+							mh3_ns
+							near_black
+							link
+							hover_blue
+							dib
+							v_btm
+							href={contributor.instagramProfile}
+							title={`Follow ${contributor.name} on Instagram`}
+						>
+							<InstagramLogo />
+						</A>
+					)}
+					{contributor.twitterProfile && (
+						<A
+							w2
+							mh3
+							near_black
+							link
+							hover_blue
+							dib
+							v_btm
+							href={contributor.twitterProfile}
+							title={`Follow ${contributor.name} on Twitter`}
+						>
+							<TwitterLogo />
+						</A>
+					)}
+					{contributor.stravaProfile && (
+						<A
+							w2
+							mh3
+							near_black
+							link
+							hover_blue
+							dib
+							v_btm
+							href={contributor.stravaProfile}
+							title={`Follow ${contributor.name} on Strava`}
+						>
+							<StravaLogo />
+						</A>
+					)}
+				</Sidebar>
+				<Profile>
+					<Heading mb3>
+						<H1 f3 f1_l fw6 lh_solid ma0>
+							{contributor.name}
+						</H1>
+					</Heading>
+					<Richtext source={contributor.bio} />
+					<Contribute />
+				</Profile>
+			</Contributor>
+		</Div>
+		<Footer />
+	</Page>
+);
 
-App.propTypes = {
+ContributorPage.propTypes = {
 	contributor: PropTypes.object
 };
 
-App.defaultProps = {
+ContributorPage.defaultProps = {
 	contributor: {
 		name: "Not found"
 	}
 };
 
-export default WithContributor(App);
+export default WithContributor(ContributorPage);
