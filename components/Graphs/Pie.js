5222e01c037c1ec78a40dd6fd8c94b9c78569934
Alex Windett <windettalex@gmail.commit>
2020-08-03 14:02:03 +0100

    working pie chart

--- components/Graphs/Pie.js
+++ components/Graphs/Pie.js
@@ -1,184 +1,114 @@
 import * as d3 from "d3";
 import { useState, useEffect } from "react";
 
-const createGraph = ({ ref, data }) => {
-	const width = 540;
-	const height = 540;
-	const radius = Math.min(width, height) / 2;
+const createGraph = ({ re, data }) => {
+	const size = 500;
+	const fourth = size / 4;
+	const half = size / 2;
+	const labelOffset = fourth * 1.4;
+	const total = data.reduce((acc, cur) => acc + cur.value, 0);
 
-	const svg = d3
+	const chart = d3
 		.select(".graph-entry")
 		.append("svg")
-		.attr("width", width)
-		.attr("height", height)
+		.attr("width", size)
+		.attr("height", size)
 		.append("g")
-		.attr("transform", `translate(${width / 2}, ${height / 2})`);
+		.attr("transform", `translate(${0}, ${0})`);
 
-	const color = d3.scaleOrdinal(d3.schemeCategory10);
+	const plotArea = chart
+		.append("g")
+		.attr("transform", `translate(${half}, ${half})`);
 
-	const pie = d3.pie().value(d => d.count);
+	const color = d3
+		.scaleOrdinal()
+		.domain(data.map(d => d.name))
+		.range([
+			"#404293",
+			"#D39647",
+			"#336487",
+			"#D3AF47",
+			"#404293",
+			"#D3C547",
+			"#5D398F",
+			"#D3AF47"
+		]);
+
+	const pie = d3
+		.pie()
+		.sort(null)
+		.value(d => d.value);
+
+	const arcs = pie(data);
 
 	const arc = d3
 		.arc()
 		.innerRadius(0)
-		.outerRadius(radius);
+		.outerRadius(fourth);
 
-	function arcTween(a) {
-		const i = d3.interpolate(this._current, a);
-		this._current = i(1);
+	const arcLabel = d3
+		.arc()
+		.innerRadius(labelOffset)
+		.outerRadius(labelOffset);
 
-		return t => arc(i(t));
-	}
+	plotArea
+		.selectAll("path")
+		.data(arcs)
+		.enter()
+		.append("path")
+		.attr("fill", d => color(d.data.name))
+		.attr("stroke", "white")
+		.attr("d", arc);
 
-	d3.selectAll(".data-update").on("change", update);
+	const labels = plotArea
+		.selectAll("text")
+		.data(arcs)
+		.enter()
+		.append("text")
+		.style("text-anchor", "middle")
+		.style("alignment-baseline", "middle")
+		.style("font-size", "20px")
+		.attr("transform", d => `translate(${arcLabel.centroid(d)})`);
 
-	function update(val = this.value) {
-		// Join new data
-		const path = svg.selectAll("path").data(pie(data[val]));
+	labels
+		.append("tspan")
+		.attr("y", "-0.6em")
+		.attr("x", 0)
+		.style("font-weight", "bold")
+		.text(d => `${d.data.name}`);
 
-		// Update existing arcs
-		path
-			.transition()
-			.duration(200)
-			.attrTween("d", arcTween);
-
-		// Enter new arcs
-		path
-			.enter()
-			.append("path")
-			.attr("fill", (d, i) => color(i))
-			.attr("d", arc)
-			.attr("stroke", "white")
-			.attr("stroke-width", "2px")
-			.each(function(d) {
-				this._current = d;
-			});
-	}
-
-	update(Object.keys(data)[0]);
+	labels
+		.append("tspan")
+		.attr("y", "0.6em")
+		.attr("x", 0)
+		.text(
+			d => `${d.data.value} (${Math.round((d.data.value / total) * 100)}%)`
+		);
 };
 
-export default ({ data, definitions }) => {
-	// const mock = {
-	// 	apples: [
-	// 		{ region: "North", count: "53245" },
-	// 		{ region: "South", count: "28479" },
-	// 		{ region: "East", count: "19697" },
-	// 		{ region: "West", count: "24037" },
-	// 		{ region: "Central", count: "40245" }
-	// 	],
-	// 	oranges: [
-	// 		{ region: "North", count: "200" },
-	// 		{ region: "South", count: "200" },
-	// 		{ region: "East", count: "200" },
-	// 		{ region: "West", count: "200" },
-	// 		{ region: "Central", count: "200" }
-	// 	]
-	// };
+export default ({ data }) => {
+	const nationalities = data.map(d => d.nationality).filter(n => n);
+	var counts = {};
 
-	const mock = {
-		result: [
-			{
-				region: "Finished",
-				count: "32"
-			},
-			{
-				region: "Scratcged",
-				count: "32"
-			}
-		],
-		nationality: [
-			{
-				region: "USA",
-				count: "25"
-			},
-			{
-				region: "ICELAND",
-				count: "1"
-			},
-			{
-				region: "IRELAND",
-				count: "1"
-			},
-			{
-				region: "FRANCE",
-				count: "5"
-			},
-			{
-				region: "AUSTRALIA",
-				count: "7"
-			},
-			{
-				region: "ITALY",
-				count: "21"
-			},
-			{
-				region: "NORWAY",
-				count: "15"
-			},
-			{
-				region: "NEW ZEALAND",
-				count: "72"
-			}
-		],
-		category: [
-			{
-				region: "Men",
-				count: "25"
-			},
-			{
-				region: "Women",
-				count: "7"
-			}
-		]
-	};
+	nationalities.forEach(x => {
+		counts[x] = (counts[x] || 0) + 1;
+	});
 
-	const dataset = mock;
+	const sumData = Object.keys(counts).map(c => ({
+		name: c,
+		value: counts[c]
+	}));
 
-	const transformedData = definitions.reduce((acc, curr) => {
-		if (!curr) return acc;
-
-		var counts = {};
-
-		let values = data.map(d => d[curr.identifier]);
-
-		// Convert values into an object where key is name, and value is occurance in array
-		values.forEach(x => {
-			counts[x] = (counts[x] || 0) + 1;
-		});
-
-		return {
-			...acc,
-			[curr.identifier]: Object.keys(counts).map(c => ({
-				region: c,
-				count: counts[c].toString()
-			}))
-		};
-	}, {});
+	if (sumData.length <= 1) return null;
 
 	useEffect(() => {
-		createGraph({ data: mock });
+		createGraph({ data: sumData });
 	}, []);
 
-	const [checked, setchecked] = useState(Object.keys(transformedData)[0]);
-
 	return (
-		<div className="graph-entry">
-			<form>
-				{definitions.map((definition, i) => (
-					<label key={definition.identifier}>
-						<input
-							className="data-update"
-							type="radio"
-							name="dataset"
-							value={definition.identifier}
-							checked={definition.identifier === checked}
-							onChange={e => setchecked(e.target.value)}
-						/>{" "}
-						{definition.label}
-					</label>
-				))}
-			</form>
+		<div>
+			<h4>Nationality</h4>
+			<div className="graph-entry"></div>
 		</div>
 	);
 };
