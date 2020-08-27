5631b68efa929ace18b3a5dc247dae4196fbf470
Alex Windett <windettalex@gmail.commit>
2020-08-27 10:00:30 +0100

    more dynamic header block layout

--- components/content-block/banner.js
+++ components/content-block/banner.js
@@ -1,23 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Block from "./banner-block";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import tachyons from "styled-components-tachyons";
 
-const Banner = ({ blocks, count }) => {
-	let template = "1fr";
-	if (count > 1) {
-		template = "1fr 1fr";
+const Grid = styled.div`
+	@media screen and (min-width: 64em) {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+
+		${props =>
+			props.count === 1 &&
+			css`
+				div:first-child {
+					grid-column: 1 / span 3;
+				}
+			`}
+
+		${props =>
+			props.count > 2 &&
+			css`
+				div:nth-child(3) {
+					grid-column: 1 / span 3;
+				}
+			`}
 	}
+	${tachyons}
+`;
 
-	const Grid = styled.div`
-		@media screen and (min-width: 64em) {
-			display: grid;
-			grid-template-columns: ${template};
-		}
-		${tachyons}
-	`;
-
+const Banner = ({ blocks, count }) => {
 	return (
 		<Grid mb5>
 			{blocks.map((block, i) => {
