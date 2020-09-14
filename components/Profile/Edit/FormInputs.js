2a4384487565aa3397fc76f1965fa589ad3bc03e
Alex Windett <windettalex@gmail.commit>
2020-09-14 08:13:04 +0100

    remove header toggle

--- components/Profile/Edit/FormInputs.js
+++ components/Profile/Edit/FormInputs.js
@@ -134,6 +134,20 @@ export default ({
 						theme="snow"
 						value={biographyValue}
 						onChange={setBiographyValue}
+						modules={{
+							toolbar: [
+								// [{ header: [1, 2, false] }],
+								["bold", "italic", "underline", "strike", "blockquote"],
+								[
+									{ list: "ordered" },
+									{ list: "bullet" },
+									{ indent: "-1" },
+									{ indent: "+1" }
+								],
+								["link", "image"],
+								["clean"]
+							]
+						}}
 					/>
 				</label>
 			</div>
