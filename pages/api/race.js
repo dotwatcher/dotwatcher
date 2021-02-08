5653a8118c460d84f8f303a16fa3aa5f89b086dd
Alex Windett <windettalex@gmail.com>
2021-02-08 19:11:08 +0000

    add console log

--- pages/api/race.js
+++ pages/api/race.js
@@ -4,6 +4,8 @@ export default async function handle(req, res) {
 	const client = await pool.connect();
 	const { slug, year } = req.query;
 
+	console.log("api races log");
+
 	try {
 		const { rows: results } = await client.query(
 			`SELECT riders.name, riders.nationality, races.name AS racename, races.year, races.description, results.position, results.cap, results.class, results.days, results.hours, results.minutes, results.result, results.bike, results.category, results.finishlocation, results.notes FROM results, riders, races WHERE riders.id = results.riderid AND races.id = results.raceid AND races.slug = '${slug}' AND races.year = ${year}`
