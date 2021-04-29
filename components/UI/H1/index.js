d26404bb2a3c6070abeed3171322f55fcfc642c5
Alex Windett <windettalex@gmail.com>
2021-04-29 08:24:04 +0100

    correct element to styled component

--- components/UI/H1/index.js
+++ components/UI/H1/index.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import mq from "@Utils/media-query";
 
-export default styled.h3`
+export default styled.h1`
 	font-size: 30px;
 	margin-top: 0;
 	font-weight: 600;
--- components/UI/H2/index.js
+++ components/UI/H2/index.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import mq from "@Utils/media-query";
 
-export default styled.h3`
+export default styled.h2`
 	font-size: 22px;
 	margin-top: 0;
 	font-weight: 600;
